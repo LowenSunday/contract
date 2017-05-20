bc455164fccca180d960af4505a8cb0916a75507
LowenSunday <luwh2000@gmail.com>
2017-05-20 20:59:10 +0800

    Rewrite the User class and Contract class in Entity
    A class path change that has to be made, but I don't know why_(
    ﾟДﾟ)ﾉSeems the JDK version is different_(:зゝ∠)_
    Rewrite the Contract class and the User class base on metheno's
    databaseヽ(✿ﾟ▽ﾟ)ノ

--- src/main/java/org/sic/contract/entity/Contract.java
+++ src/main/java/org/sic/contract/entity/Contract.java
@@ -1,58 +1,103 @@
 package org.sic.contract.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
-public class Contract implements Serializable{
-	
+public class Contract implements Serializable
+{
 	private static final long serialVersionUID = 351953605173085300L;
-	
-	private Long id;
-	
-	private String contractName;
-	
-	private String fileUri;
-	
-	private String md5;
-	
-	private String createTime;
+	private static long cid;
+	private static String ctype;
+	private static long uid;
+	private static Date date_start;
+	private static Date date_end;
+	private static long contract_length;
+	private static long trail_length;
+	private static int hand_book_exist;
 
-	public Long getId() {
-		return id;
+	public long getCid()
+	{
+		return cid;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setCid(long cid)
+	{
+		Contract.cid = cid;
 	}
 
-	public String getContractName() {
-		return contractName;
+	public static String getCtype()
+	{
+		return ctype;
 	}
 
-	public void setContractName(String contractName) {
-		this.contractName = contractName;
+	public static void setCtype(String ctype)
+	{
+		Contract.ctype = ctype;
 	}
 
-	public String getFileUri() {
-		return fileUri;
+	public static long getUid()
+	{
+		return uid;
 	}
 
-	public void setFileUri(String fileUri) {
-		this.fileUri = fileUri;
+	public static void setUid(long uid)
+	{
+		Contract.uid = uid;
 	}
 
-	public String getMd5() {
-		return md5;
+	public static Date getDate_start()
+	{
+		return date_start;
 	}
 
-	public void setMd5(String md5) {
-		this.md5 = md5;
+	public static void setDate_start(Date date_start)
+	{
+		Contract.date_start = date_start;
 	}
 
-	public String getCreateTime() {
-		return createTime;
+	public static Date getDate_end()
+	{
+		return date_end;
 	}
 
-	public void setCreateTime(String createTime) {
-		this.createTime = createTime;
+	public static void setDate_end(Date date_end)
+	{
+		Contract.date_end = date_end;
 	}
+
+	public static long getContract_length()
+	{
+		return contract_length;
+	}
+
+	public static void setContract_length(long contract_length)
+	{
+		Contract.contract_length = contract_length;
+	}
+
+	public static long getTrail_length()
+	{
+		return trail_length;
+	}
+
+	public static void setTrail_length(long trail_length)
+	{
+		Contract.trail_length = trail_length;
+	}
+
+	public static int getHand_book_exist()
+	{
+		return hand_book_exist;
+	}
+
+	public static void setHand_book_exist(int hand_book_exist)
+	{
+		Contract.hand_book_exist = hand_book_exist;
+	}
+
+	public static long getSerialversionuid()
+	{
+		return serialVersionUID;
+	}
+
 }
--- src/main/java/org/sic/contract/entity/User.java
+++ src/main/java/org/sic/contract/entity/User.java
@@ -1,68 +1,280 @@
 package org.sic.contract.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
-public class User implements Serializable{
-	
+public class User implements Serializable
+{
+
 	private static final long serialVersionUID = 6874476411700853379L;
+	private static long uid;
+	private static String utype;
+	private static String emall;
+	private static String password;
+	private static String remember_token;
+	private static Date created_at;
+	private static Date updated_at;
+	private static String surname;
+	private static String first_name;
+	private static String gender;
+	private static String nationality;
+	private static String political_status;
+	private static Date birthday;
+	private static String china_id_num;
+	private static String edu_stats;
+	private static String graduated_from;
+	private static String hj_location;
+	private static String hk_type;
+	private static String hk_address;
+	private static String hk_postcode;
+	private static String now_address;
+	private static String now_postcode;
+	private static String mobile_num;
+	private static String tele_num;
 
-	private Long id;
-	
-	private Long companyId;
-	
-	private String userName;
-	
-	private String password;
-	
-	private String email;
-	
-	private String phone;
-
-	public Long getId() {
-		return id;
+	public static long getUid()
+	{
+		return uid;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public static void setUid(long uid)
+	{
+		User.uid = uid;
 	}
 
-	public Long getCompanyId() {
-		return companyId;
+	public static String getUtype()
+	{
+		return utype;
 	}
 
-	public void setCompanyId(Long companyId) {
-		this.companyId = companyId;
+	public static void setUtype(String utype)
+	{
+		User.utype = utype;
 	}
 
-	public String getUserName() {
-		return userName;
+	public static String getEmall()
+	{
+		return emall;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public static void setEmall(String emall)
+	{
+		User.emall = emall;
 	}
 
-	public String getPassword() {
+	public static String getPassword()
+	{
 		return password;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public static void setPassword(String password)
+	{
+		User.password = password;
 	}
 
-	public String getEmail() {
-		return email;
+	public static String getRemember_token()
+	{
+		return remember_token;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public static void setRemember_token(String remember_token)
+	{
+		User.remember_token = remember_token;
 	}
 
-	public String getPhone() {
-		return phone;
+	public static Date getCreated_at()
+	{
+		return created_at;
 	}
 
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public static void setCreated_at(Date created_at)
+	{
+		User.created_at = created_at;
 	}
+
+	public static Date getUpdated_at()
+	{
+		return updated_at;
+	}
+
+	public static void setUpdated_at(Date updated_at)
+	{
+		User.updated_at = updated_at;
+	}
+
+	public static String getSurname()
+	{
+		return surname;
+	}
+
+	public static void setSurname(String surname)
+	{
+		User.surname = surname;
+	}
+
+	public static String getFirst_name()
+	{
+		return first_name;
+	}
+
+	public static void setFirst_name(String first_name)
+	{
+		User.first_name = first_name;
+	}
+
+	public static String getGender()
+	{
+		return gender;
+	}
+
+	public static void setGender(String gender)
+	{
+		User.gender = gender;
+	}
+
+	public static String getNationality()
+	{
+		return nationality;
+	}
+
+	public static void setNationality(String nationality)
+	{
+		User.nationality = nationality;
+	}
+
+	public static String getPolitical_status()
+	{
+		return political_status;
+	}
+
+	public static void setPolitical_status(String political_status)
+	{
+		User.political_status = political_status;
+	}
+
+	public static Date getBirthday()
+	{
+		return birthday;
+	}
+
+	public static void setBirthday(Date birthday)
+	{
+		User.birthday = birthday;
+	}
+
+	public static String getChina_id_num()
+	{
+		return china_id_num;
+	}
+
+	public static void setChina_id_num(String china_id_num)
+	{
+		User.china_id_num = china_id_num;
+	}
+
+	public static String getEdu_stats()
+	{
+		return edu_stats;
+	}
+
+	public static void setEdu_stats(String edu_stats)
+	{
+		User.edu_stats = edu_stats;
+	}
+
+	public static String getGraduated_from()
+	{
+		return graduated_from;
+	}
+
+	public static void setGraduated_from(String graduated_from)
+	{
+		User.graduated_from = graduated_from;
+	}
+
+	public static String getHj_location()
+	{
+		return hj_location;
+	}
+
+	public static void setHj_location(String hj_location)
+	{
+		User.hj_location = hj_location;
+	}
+
+	public static String getHk_type()
+	{
+		return hk_type;
+	}
+
+	public static void setHk_type(String hk_type)
+	{
+		User.hk_type = hk_type;
+	}
+
+	public static String getHk_address()
+	{
+		return hk_address;
+	}
+
+	public static void setHk_address(String hk_address)
+	{
+		User.hk_address = hk_address;
+	}
+
+	public static String getHk_postcode()
+	{
+		return hk_postcode;
+	}
+
+	public static void setHk_postcode(String hk_postcode)
+	{
+		User.hk_postcode = hk_postcode;
+	}
+
+	public static String getNow_address()
+	{
+		return now_address;
+	}
+
+	public static void setNow_address(String now_address)
+	{
+		User.now_address = now_address;
+	}
+
+	public static String getNow_postcode()
+	{
+		return now_postcode;
+	}
+
+	public static void setNow_postcode(String now_postcode)
+	{
+		User.now_postcode = now_postcode;
+	}
+
+	public static String getMobile_num()
+	{
+		return mobile_num;
+	}
+
+	public static void setMobile_num(String mobile_num)
+	{
+		User.mobile_num = mobile_num;
+	}
+
+	public static String getTele_num()
+	{
+		return tele_num;
+	}
+
+	public static void setTele_num(String tele_num)
+	{
+		User.tele_num = tele_num;
+	}
+
+	public static long getSerialversionuid()
+	{
+		return serialVersionUID;
+	}
+
 }
